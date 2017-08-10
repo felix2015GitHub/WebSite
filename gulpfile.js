2bc6bab65106c2e8608a48a9c94f1a8ce9e6aaa1
felix <felix.cheng@latticeworkinc.com>
2017-08-10 17:22:42 +0800

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-var gulp      = require('gulp'),
-  del         = require('del'),
-  concat      = require('gulp-concat'),
-  minifyCSS   = require('gulp-minify-css'),
-  uglify      = require('gulp-uglify'),
-  rename      = require("gulp-rename"),
-  htmlreplace = require('gulp-html-replace'),
-  minifyHTML  = require('gulp-minify-html');
+var gulp        = require('gulp'),
+    del         = require('del'),
+    concat      = require('gulp-concat'),
+    minifyCSS   = require('gulp-minify-css'),
+    uglify      = require('gulp-uglify'),
+    rename      = require("gulp-rename"),
+    htmlreplace = require('gulp-html-replace'),
+    minifyHTML  = require('gulp-minify-html');
 
 gulp.task('concat', function() {
   return gulp.src('./app/css/*.css')
     .pipe(concat('app.css'))
-    .pipe(gulp.dest('./build/css/'));
+    .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('minify-css', ['concat'], function() {
-  return gulp.src('./build/css/app.css')
+  return gulp.src('./dist/css/app.css')
     .pipe(minifyCSS({
       keepBreaks: true,
     }))
@@ -22,29 +22,29 @@ gulp.task('minify-css', ['concat'], function() {
       path.basename += ".min";
       path.extname = ".css";
     }))
-    .pipe(gulp.dest('./build/css/'));
+    .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('images', function(cb) {
   return gulp.src('./app/img/*.{png,jpg,jpeg,gif}')
-    .pipe(gulp.dest('./build/img/'));
+    .pipe(gulp.dest('./dist/img/'));
 });
 
 gulp.task('fonts', function(cb) {
   return gulp.src('./app/fonts/*')
-    .pipe(gulp.dest('./build/fonts/'));
+    .pipe(gulp.dest('./dist/fonts/'));
 });
 
 gulp.task('libs', function(cb) {
   return gulp.src('./app/libs/*')
-    .pipe(gulp.dest('./build/libs/'));
+    .pipe(gulp.dest('./dist/libs/'));
 });
 
 gulp.task('uglify', function() {
   return gulp.src('./app/js/*.js')
     .pipe(concat('app.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./build/js/'));
+    .pipe(gulp.dest('./dist/js/'));
 });
 
 gulp.task('html-replace',function() {
@@ -55,11 +55,11 @@ gulp.task('html-replace',function() {
         'js': 'js/app.min.js'
     }))  
     .pipe(minifyHTML(opts))
-    .pipe(gulp.dest('./build/'));
+    .pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('clean', function(cb) {
-  return del('build', cb);
+  return del('dist', cb);
 });
 
 gulp.task('default', ['html-replace','minify-css', 'images', 'fonts', 'libs', 'uglify']);
